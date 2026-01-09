24da0734a87157555f0ec30441fd8bf3a81ff47a
Anushri <anushri_gupta@outlook.com>
2026-01-09 14:11:26 +0100

    template

---
+++ rice_ops/src/rice_ops/train.py
@@ -0,0 +1,10 @@
+from rice_ops.model import Model
+from rice_ops.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
